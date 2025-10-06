3efa94984556f69cb2dd7438d263d9e830aca485
dchavezm6 <jdcm745@gmail.com>
2025-10-06 04:23:47 -0600

    Fix: Corregir definitivamente comando create_service_area

--- apps/requests/management/commands/create_service_areas.py
+++ apps/requests/management/commands/create_service_areas.py
@@ -6,38 +6,38 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         areas = [
-            'Cantón Camache chiquito, Sector Godínes'
-            'Cantón Camache chiquito, Sector Patricio'
-            'Cantón Camache chiquito, Sector Ramirez'
-            'Cantón Camache chiquito, Sector Silvestre'
-            'Cantón Camache Grande Centro'
-            'Cantón Camache Grande, Sector Camino'
-            'Cantón Maza, San  Rubén'
-            'Cantón San Antoñito'
-            'Casco Urbano: Barrio la Unión'
-            'Casco Urbano: Barrio Pobre'
-            'Casco Urbano: Barrio Rico'
-            'Casco Urbano: Colonia el Rastro'
-            'Casco Urbano: Colonia el Tigre'
-            'Casco Urbano: Colonia Juárez'
-            'Casco Urbano: Colonia la Bendición'
-            'Casco Urbano: Colonia Margarita'
-            'Casco Urbano: Colonia San Francisco'
-            'Casco Urbano: La Alameda'
-            'Casco Urbano: La Cuchilla'
-            'Casco Urbano: Monjón'
-            'Casco Urbano: Sector Claveles'
-            'Casco Urbano: Sector Maxeño'
-            'Chirij Sin'
-            'Loma larga'
-            'Pabayal I'
-            'Pabayal II'
-            'Pabayal III'
-            'San Juan Maza'
-            'Sector el Carmen'
-            'Sector Vásquez'
+    'Cantón Camache chiquito, Sector Godínes'
+'Cantón Camache chiquito, Sector Patricio'
+'Cantón Camache chiquito, Sector Ramirez'
+'Cantón Camache chiquito, Sector Silvestre'
+'Cantón Camache Grande Centro'
+'Cantón Camache Grande, Sector Camino'
+'Cantón Maza, San  Rubén'
+'Cantón San Antoñito'
+'Casco Urbano: Barrio la Unión'
+'Casco Urbano: Barrio Pobre'
+'Casco Urbano: Barrio Rico'
+'Casco Urbano: Colonia el Rastro'
+'Casco Urbano: Colonia el Tigre'
+'Casco Urbano: Colonia Juárez'
+'Casco Urbano: Colonia la Bendición'
+'Casco Urbano: Colonia Margarita'
+'Casco Urbano: Colonia San Francisco'
+'Casco Urbano: La Alameda'
+'Casco Urbano: La Cuchilla'
+'Casco Urbano: Monjón'
+'Casco Urbano: Sector Claveles'
+'Casco Urbano: Sector Maxeño'
+'Chirij Sin'
+'Loma larga'
+'Pabayal I'
+'Pabayal II'
+'Pabayal III'
+'San Juan Maza'
+'Sector el Carmen'
+'Sector Vásquez'
         ]
-
+        
         created = 0
         for area_name in areas:
             area, created_now = ServiceArea.objects.get_or_create(
@@ -49,7 +49,7 @@ class Command(BaseCommand):
                 self.stdout.write(f"✓ Creado: {area.name}")
             else:
                 self.stdout.write(f"- Ya existe: {area.name}")
-
+        
         self.stdout.write(
             self.style.SUCCESS(f'\nTotal: {created} áreas de servicio creadas')
         )
